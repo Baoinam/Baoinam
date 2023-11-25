32ecc7259d1cae87f3425bac9bf1f0697b02f5b5
Baoinam <151725722+Baoinam@users.noreply.github.com>
2023-11-26 01:28:15 +0800

    Update ImageViewer.py

--- ImageViewer.py
+++ ImageViewer.py
@@ -9,6 +9,7 @@ root.geometry('500x500')
 root.title('FotoFoto')
 
 
+
 def popup_menu(e):
     menu_bar.tk_popup(x=e.x_root, y=e.y_root)
 
@@ -39,23 +40,24 @@ def load_images():
         globals()[image_vars[n]].pack(side=tk.LEFT)
 
 
+filename = "EditingPage.py"
 def open_editor_page():
     root.destroy()
-    os.system("EditingPage.py")
+    os.system(f"python {filename}")
+    
 
 
 # Buttons
-
-editor_page_btn = tk.Button(root, text='Edit', bd=0, font=('Bold', 15),
-                            command=open_editor_page)
-editor_page_btn.pack(side=tk.TOP, anchor=tk.W, pady=20, padx=20)
-
 menu_btn = tk.Button(root, text='Open Folder', bd=0, font=('Bold', 15))
 menu_btn.pack(side=tk.TOP, anchor=tk.W, pady=10, padx=20)
 menu_btn.bind('<Button-1>', popup_menu)
 
 menu_bar = tk.Menu(root, tearoff=False)
 menu_bar.add_command(label='Open Folder', command=load_images)
+
+editor_page_btn = tk.Button(root, text='Edit', bd=0, font=('Bold', 15),
+                            command=open_editor_page)
+editor_page_btn.pack(side=tk.TOP, anchor=tk.W, pady=20, padx=20)
 
 image_display_lb = tk.Label(root)
 image_display_lb.pack(anchor=tk.CENTER)
